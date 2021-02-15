340a38c101a1f26b03d445385a47da279e71baac
ve-sdk-ios <73183216+ve-sdk-ios@users.noreply.github.com>
2021-02-15 13:22:19 +0300

    Update BanubaClientToken.swift

--- FaceAR-VE-Sample/FaceAR-VE-Sample/BanubaClientToken.swift
+++ FaceAR-VE-Sample/FaceAR-VE-Sample/BanubaClientToken.swift
@@ -4,4 +4,4 @@ import UIKit
 internal let banubaClientToken = <#Place your token here#>
 
 /// Video Editor effects token
-internal let vidoEditorEffectsToken = <#Place your token here#>
+internal let videoEditorEffectsToken = "x0b/NssBRnErNyByyPbYFyxK7fK+1u0DuoAruXXhLZ9+RJh+2D4s9ZoGaGaACFfgi0HAL32wzC1slfUqZ6yyxXpxQ+ljiL+/hFpStDMmBWGZBCvG+FlTtd9wa9bU9rC6DjJwNuRfGSe4bszdKxGBEPEsZoZqIQ=="
