b395feee56de96a303dee4eeaf9d257a3fbda775
ve-sdk-ios <73183216+ve-sdk-ios@users.noreply.github.com>
2021-02-15 16:13:13 +0300

    FaceAR+VideoEditor (#1)
    * initial commit
    
    * Update BanubaClientToken.swift
    
    * Update ViewController.swift
    
    Co-authored-by: Gleb Markin <gleb.markin@banuba.com>

---
+++ FaceAR-VE-Sample/FaceAR-VE-Sample/BanubaClientToken.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+/// Face AR token
+internal let banubaClientToken = <#Place your token here#>
+
+/// Video Editor effects token
+internal let videoEditorEffectsToken = "x0b/NssBRnErNyByyPbYFyxK7fK+1u0DuoAruXXhLZ9+RJh+2D4s9ZoGaGaACFfgi0HAL32wzC1slfUqZ6yyxXpxQ+ljiL+/hFpStDMmBWGZBCvG+FlTtd9wa9bU9rC6DjJwNuRfGSe4bszdKxGBEPEsZoZqIQ=="
